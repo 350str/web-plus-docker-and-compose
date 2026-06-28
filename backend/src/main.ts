fb3b5c7451857b5af58edf3a6634bc6a731f4221
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-28 18:45:07 +0700

    ready to push

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:8081', 'http://127.0.0.1:8081'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
       'Origin',
